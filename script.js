d52bd11060f38f6caf2d0bd0dfebe60be7134a0c
404-cee-jay <181482971+404-cee-jay@users.noreply.github.com>
2026-03-16 21:22:58 +0300

    the product card

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.getElementById('customCursor');
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-    cursor.style.display = 'block';
-});
+
+
